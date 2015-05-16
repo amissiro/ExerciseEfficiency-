f8a9fa8ede8b0151ff97fad4e2ae5363c9976dee
Muzamil <muzzimsyed21@gmail.com>
2015-05-16 14:02:18 -0700

    minor update

--- index.php
+++ index.php
@@ -25,16 +25,15 @@
 <body>
 
 <center>
-<h1>Welcome To The Exercise Efficiency Application</h1>
+<h1>Welcome To The Exercise Efficiency App!</h1>
 
-<center>
-<h1>Choose what do you wanna do today</h1>
+<h1>Choose which from of exercises you would like to do today</h1>
 <button id="b1">CARDIO</button>
 <br>
 <br>
 <br>
 <button id="b2">WEIGHT LIFTING</button>
-</center>
+
 
 </center>
 </body>
