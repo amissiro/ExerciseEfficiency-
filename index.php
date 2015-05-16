f27034f0a7e22cd7168aac4157d169c9c5b5408d
Muzamil <muzzimsyed21@gmail.com>
2015-05-16 14:21:36 -0700

    new files and server side include

--- index.php
+++ index.php
@@ -1,28 +1,4 @@
-<!DOCTYPE html>
-<html>
-<head>
-  <meta charset="utf-8">
-
-  <title>ExerciseEfficiency</title>
-  <meta name="description" content="My Parse App">
-  <meta name="viewport" content="width=device-width">
-  <!--<link rel='stylesheet' type='text/css' href='stylesheet.css'/>-->
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
-  <!-- Latest compiled and minified CSS -->
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css">
-  <!-- Optional theme -->
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap-theme.min.css">
-  
-  <!-- Latest compiled and minified JavaScript -->
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
-  <script type="text/javascript" src="https://www.parsecdn.com/js/parse-1.2.18.min.js"></script>
-
-  <link rel="stylesheet" href="css/style.css">
-  <script type = "text/javascript" src="js/javascript.js"></script>
-
-</head>
-
-<body>
+<!--#include file="topPage.php" -->
 
 <center>
 <h1>Welcome To The Exercise Efficiency App!</h1>
@@ -39,4 +15,5 @@
 </body>
 </html>
 
-  
+
+<!--#include file="bottomPage.php" -->
---
+++ topPage.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+
+  <title>ExerciseEfficiency</title>
+  <meta name="description" content="My Parse App">
+  <meta name="viewport" content="width=device-width">
+  <!--<link rel='stylesheet' type='text/css' href='stylesheet.css'/>-->
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
+  <!-- Latest compiled and minified CSS -->
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css">
+  <!-- Optional theme -->
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap-theme.min.css">
+  
+  <!-- Latest compiled and minified JavaScript -->
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
+  <script type="text/javascript" src="https://www.parsecdn.com/js/parse-1.2.18.min.js"></script>
+
+  <link rel="stylesheet" href="css/style.css">
+  <script type = "text/javascript" src="js/javascript.js"></script>
+
+</head>
+
+<body>
