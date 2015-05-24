13514d73c345cf8188d279060ee6482bde596eb9
Muzamil <muzzimsyed21@gmail.com>
2015-05-23 17:07:20 -0700

    modularized files

--- index.php
+++ index.php
@@ -1,54 +1,27 @@
-<!--Adding ServerSideInclude later" -->
-
-<!DOCTYPE html>
-<html>
-<head>
-  <meta charset="utf-8">
-
-  <title>ExerciseEfficiency</title>
-  <meta name="description" content="My Parse App">
-  <meta name="viewport" content="width=device-width">
-  <!--<link rel='stylesheet' type='text/css' href='stylesheet.css'/>-->
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
-  <!-- Latest compiled and minified CSS -->
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css">
-  <!-- Optional theme -->
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap-theme.min.css">
-  
-  <!-- Latest compiled and minified JavaScript -->
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
-  <script type="text/javascript" src="https://www.parsecdn.com/js/parse-1.2.18.min.js"></script>
-
-  <link rel="stylesheet" href="css/style.css">
-  <script type = "text/javascript" src="js/javascript.js"></script>
-
-</head>
-
-<body>
+<?php include 'includes/topPage.php'; ?>
 
 <center>
-This will be the login page
-<form>
-  <div class="form-group">
-    <label for="exampleInputEmail1">Email address</label>
-    <input type="email" class="form-control" id="exampleInputEmail1" placeholder="Enter email">
+Welcome to the Exercise Efficiency Calendar!
+
+
+  <form class="form-signin" method="post" action="">
+
+    <h1 class="form-signin-heading text-muted">Sign In</h1>
+    
+    <input type="text" class="form-control" placeholder="Username" name = "username">
+    <input type="password" class="form-control" placeholder="Password" name = "psw">
+   
+    <button class="btn btn-lg btn-primary btn-block" type="submit">
+      Sign In
+    </button>
+    
+    <br><br><br>
+  <div class="alert alert-danger" role="alert" id="error" style="visibility:hidden">
+      <span class="glyphicon glyphicon-exclamation-sign" aria-hidden="true"></span>
+      <span class="sr-only">Error:</span>
+        Enter valid login information
   </div>
-  <div class="form-group">
-    <label for="exampleInputPassword1">Password</label>
-    <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
-  </div>
-  <div class="form-group">
-    <label for="exampleInputFile">File input</label>
-    <input type="file" id="exampleInputFile">
-    <p class="help-block">Example block-level help text here.</p>
-  </div>
-  <div class="checkbox">
-    <label>
-      <input type="checkbox"> Check me out
-    </label>
-  </div>
-  <button type="submit" class="btn btn-default">Submit</button>
-</form>
+   </form>
 
 </center>
 
--- option.php
+++ option.php
@@ -27,15 +27,13 @@
 <body>
 
 <center>
-<h1>Welcome To The Exercise Efficiency App!</h1>
+<h1>Exercise Efficiency Calendar</h1>
 
-<h1>Choose which from of exercises you would like to do today</h1>
-<button id="btn1" onClick="parent.location='selectDay.php'">CARDIO</button>
-<br>
-<br>
-<br>
-<br>
-<button id="btn2" onClick="parent.location='selectDay.php'">WEIGHT LIFTING</button>
+<h1>Choose an option:</h1>
+<br><br>
+
+<button id="btn1" onClick="parent.location='selectDay.php'">Create A New Exercise Schedule</button> &nbsp&nbsp &nbsp 
+<button id="btn2" onClick="parent.location='calendar.php'">View/Edit Current Exercise Calendar</button>
 
 
 </center>
