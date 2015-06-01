cdcba705cdf3e21bc8f0274871d50f4093d6c378
azret <amissiro@uci.edu>
2015-06-01 00:34:19 -0700

    added some buttons for cardio

--- web/selectDay.php
+++ web/selectDay.php
@@ -39,11 +39,11 @@
           <section class="icon fa fa-bolt" style="text-align: center; "><span class="title">ExcerciseEfficiency</span></section>
         </div>
         
-        <div class="page" id="pm3">
+        <div class="page" id="pm3" style="margin:auto; max-width: 60em;">
           <section class="icon"><span class="title" ></span>
 			<button class="btn btn-3 btn-sep icon-heart">Cardio</button>
 			<button class="btn btn-4 btn-sep icon-dumbbell">Weights</button>
-          <div class="thumbnail-grid flex" style="margin:auto">
+          <div class="thumbnail-grid flex">
   <a href="https://sinfonietta-meridiana.de/" target="_blank" class="flex-item">
     <figure class="i1">
       <figcaption>Running</figcaption>
