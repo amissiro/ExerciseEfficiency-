6d9852ca64905a3dcfcda4cc896d444da96d8d77
azret <amissiro@uci.edu>
2015-06-02 00:22:56 -0700

    added reset

--- web/js/switchViews.js
+++ web/js/switchViews.js
@@ -1,12 +1,10 @@
  
     function showCardio(){ 
-       document.getElementById("stollcardio").style.display="none";
        document.getElementById("stollweights").style.display="none";
        document.getElementById("stollcardio").style.display="initial";
     }
  
     function showWeights(){ 
-       document.getElementById("stollweights").style.display="none";
        document.getElementById("stollcardio").style.display="none";
        document.getElementById("stollweights").style.display="initial";
     }
