7c3607ffb23b0e5ee07630fe2418d84271605b46
Muzamil <muzzimsyed21@gmail.com>
2015-06-01 22:37:31 -0700

    icons page

--- web/selectDay.php
+++ web/selectDay.php
@@ -54,105 +54,7 @@
         
         <!-- Monday -->
         <div class="page" id="pm2">
-
-
-          <section class="icon"><span class="title" ></span>
-      <button class="btn btn-3 btn-sep icon-heart" onclick="showCardio()">Cardio</button>
-      <button class="btn btn-4 btn-sep icon-dumbbell" onclick="showWeights()">Weights</button>
-  
-<div id="stollcardio" style="display:none">
-  <div class="thumbnail-grid flex">
-  <a href="https://sinfonietta-meridiana.de/" target="_blank" class="flex-item">
-    <figure class="i1">
-      <figcaption>Running</figcaption>
-    </figure>
-  </a>
-  <a href="https://pension-am-alten-waschhaus.de/en/" target="_blank" class="flex-item">
-    <figure class="i2">
-      <figcaption>Swimming</figcaption>
-    </figure>
-  </a>
-  <a href="https://goerlitzmade.com/en/" target="_blank" class="flex-item">
-    <figure class="i3">
-      <figcaption>Jogging</figcaption>
-    </figure>
-  </a>
-   <a href="https://goerlitzmade.com/en/" target="_blank" class="flex-item">
-    <figure class="i4">
-      <figcaption>Bicycle</figcaption>
-    </figure>
-  </a>
-     <a href="https://goerlitzmade.com/en/" target="_blank" class="flex-item">
-    <figure class="i5">
-      <figcaption>Boxing</figcaption>
-    </figure>
-  </a>
-  <a href="https://goerlitzmade.com/en/" target="_blank" class="flex-item">
-    <figure class="i6">
-      <figcaption>Walking</figcaption>
-    </figure>
-  </a>
-       <a href="https://goerlitzmade.com/en/" target="_blank" class="flex-item">
-    <figure class="i7">
-      <figcaption>Jump Rope</figcaption>
-    </figure>
-  </a>
-   <a href="https://goerlitzmade.com/en/" target="_blank" class="flex-item">
-    <figure class="i8">
-      <figcaption>Yoga</figcaption>
-    </figure>
-  </a>
- </div>
-</div>
-
-<!--Weights-->
-
-  <div id = "stollweights" style="display:none">
-   <div class="thumbnail-grid flex">
-     <a href="https://goerlitzmade.com/en/" target="_blank" class="flex-item">
-     <figure class="i9">
-       <figcaption>Chest</figcaption>
-     </figure>
-     </a>
-     <a href="https://goerlitzmade.com/en/" target="_blank" class="flex-item">
-     <figure class="i10">
-       <figcaption>Legs</figcaption>
-     </figure>
-     </a>
-     <a href="https://goerlitzmade.com/en/" target="_blank" class="flex-item">
-     <figure class="i11">
-       <figcaption>Arms</figcaption>
-     </figure>
-     </a>
-     <a href="https://goerlitzmade.com/en/" target="_blank" class="flex-item">
-     <figure class="i12">
-       <figcaption>Shoulders</figcaption>
-     </figure>
-     </a>
-     <a href="https://goerlitzmade.com/en/" target="_blank" class="flex-item">
-     <figure class="i13">
-       <figcaption>LowerBack</figcaption>
-     </figure>
-     </a>
-     <a href="https://goerlitzmade.com/en/" target="_blank" class="flex-item">
-     <figure class="i14">
-       <figcaption>UpperBack</figcaption>
-     </figure>
-     </a>
-     <a href="https://goerlitzmade.com/en/" target="_blank" class="flex-item">
-     <figure class="i15">
-       <figcaption>Abs</figcaption>
-     </figure>
-     </a>
-     <a href="https://goerlitzmade.com/en/" target="_blank" class="flex-item">
-     <figure class="i16">
-       <figcaption>Neck</figcaption>
-     </figure>
-     </a>
-    </div>
-  </div>
- </section>
-
+          <?php include('icons.php'); ?>
         </div>
   
 
@@ -160,103 +62,6 @@
         <!-- Tuesday -->
         <div class="page" id="pm1">
 
-
-<section class="icon"><span class="title" ></span>
-      <button class="btn btn-3 btn-sep icon-heart" onclick="showCardio()">Cardio</button>
-      <button class="btn btn-4 btn-sep icon-dumbbell" onclick="showWeights()">Weights</button>
-  
-<div id="stollcardio" style="display:none">
-  <div class="thumbnail-grid flex">
-  <a href="https://sinfonietta-meridiana.de/" target="_blank" class="flex-item">
-    <figure class="i1">
-      <figcaption>Running</figcaption>
-    </figure>
-  </a>
-  <a href="https://pension-am-alten-waschhaus.de/en/" target="_blank" class="flex-item">
-    <figure class="i2">
-      <figcaption>Swimming</figcaption>
-    </figure>
-  </a>
-  <a href="https://goerlitzmade.com/en/" target="_blank" class="flex-item">
-    <figure class="i3">
-      <figcaption>Jogging</figcaption>
-    </figure>
-  </a>
-   <a href="https://goerlitzmade.com/en/" target="_blank" class="flex-item">
-    <figure class="i4">
-      <figcaption>Bicycle</figcaption>
-    </figure>
-  </a>
-     <a href="https://goerlitzmade.com/en/" target="_blank" class="flex-item">
-    <figure class="i5">
-      <figcaption>Boxing</figcaption>
-    </figure>
-  </a>
-  <a href="https://goerlitzmade.com/en/" target="_blank" class="flex-item">
-    <figure class="i6">
-      <figcaption>Walking</figcaption>
-    </figure>
-  </a>
-       <a href="https://goerlitzmade.com/en/" target="_blank" class="flex-item">
-    <figure class="i7">
-      <figcaption>Jump Rope</figcaption>
-    </figure>
-  </a>
-   <a href="https://goerlitzmade.com/en/" target="_blank" class="flex-item">
-    <figure class="i8">
-      <figcaption>Yoga</figcaption>
-    </figure>
-  </a>
- </div>
-</div>
-
-<!--Weights-->
-
-  <div id = "stollweights" style="display:none">
-   <div class="thumbnail-grid flex">
-     <a href="https://goerlitzmade.com/en/" target="_blank" class="flex-item">
-     <figure class="i9">
-       <figcaption>Chest</figcaption>
-     </figure>
-     </a>
-     <a href="https://goerlitzmade.com/en/" target="_blank" class="flex-item">
-     <figure class="i10">
-       <figcaption>Legs</figcaption>
-     </figure>
-     </a>
-     <a href="https://goerlitzmade.com/en/" target="_blank" class="flex-item">
-     <figure class="i11">
-       <figcaption>Arms</figcaption>
-     </figure>
-     </a>
-     <a href="https://goerlitzmade.com/en/" target="_blank" class="flex-item">
-     <figure class="i12">
-       <figcaption>Shoulders</figcaption>
-     </figure>
-     </a>
-     <a href="https://goerlitzmade.com/en/" target="_blank" class="flex-item">
-     <figure class="i13">
-       <figcaption>LowerBack</figcaption>
-     </figure>
-     </a>
-     <a href="https://goerlitzmade.com/en/" target="_blank" class="flex-item">
-     <figure class="i14">
-       <figcaption>UpperBack</figcaption>
-     </figure>
-     </a>
-     <a href="https://goerlitzmade.com/en/" target="_blank" class="flex-item">
-     <figure class="i15">
-       <figcaption>Abs</figcaption>
-     </figure>
-     </a>
-     <a href="https://goerlitzmade.com/en/" target="_blank" class="flex-item">
-     <figure class="i16">
-       <figcaption>Neck</figcaption>
-     </figure>
-     </a>
-    </div>
-  </div>
- </section>
         </div> 
 
 
@@ -283,7 +88,7 @@
         
         <!-- Sunday -->
         <div class="page" id="p4">
-          
+        
         </div>
 
      </div>
